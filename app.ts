229078d672856df4566782f3691b1b7417d2f590
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2025-12-28 20:59:26 +0100

    feat: add requestHandler

--- app.ts
+++ app.ts
@@ -8,6 +8,18 @@ import Applauncher from './widgets/Applauncher'
 app.start({
   css: style,
   icons: './icons',
+  requestHandler(argv, res) {
+    const [cmd, arg] = argv
+    console.log(cmd)
+    switch (cmd) {
+      case 'toggle':
+        const w = app.get_window(arg)
+        if (w) w.visible = !w.visible
+        return res('ok')
+      default:
+        return res('unknown command')
+    }
+  },
   main() {
     app.get_monitors().map(Bar)
     Osd()
